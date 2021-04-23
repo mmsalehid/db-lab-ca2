c978913f31079478ca19e3c37f242b5e0561e8ea
mohammad mahdi salehi <msalehi20@gmail.com>
2021-04-23 16:32:20 +0430

    Add entities and dtos for sample typeorm project

---
+++ src/db/genre.entity.ts
@@ -0,0 +1,12 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export default class GenreEntity extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: string;
+
+}
