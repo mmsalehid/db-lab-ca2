9f8efc1fe614ddaf7d7cdec23d889f3b03716c77
mohammad mahdi salehi <msalehi20@gmail.com>
2021-04-23 17:27:39 +0430

    Add sample typeorm project apis

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { BooksService } from './books.service';
+import CreateBookDto from './dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+    constructor(private readonly booksService: BooksService){}
+
+    @Post('post')
+    postBook ( @Body() book: CreateBookDto) {
+        return this.booksService.insert(book);
+    }
+
+    @Get()
+    getAll (){
+        return this.booksService.getAllBooks();
+    }
+
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import CreateUserDto from './dto/create-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly usersServices: UserService) {}
+
+  @Post('post')
+  postUser( @Body() user: CreateUserDto) {
+    return this.usersServices.insert(user);
+  }
+// 'getAll()' returns the list of all the existing users in the database
+  @Get()
+  getAll() {
+    return this.usersServices.getAllUsers();
+  }
+
+//'getBooks()' return all the books which are associated with the user 
+// provided through 'userID' by the request  
+  @Get('books')
+  getBooks( @Body('userID', ParseIntPipe) userID: number ) {
+    return this.usersServices.getBooksOfUser(userID);
+  }
+}
